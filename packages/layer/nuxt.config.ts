99fff8ced487bb085e3b24919301511586cc2a46
Nicole Hollant <nicolehollant@gmail.com>
2023-05-21 18:24:39 -0400

    relative paths

--- packages/layer/nuxt.config.ts
+++ packages/layer/nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   modules: ['nuxt-icon', '@nuxt-fable/module'],
   srcDir: './src',
   build: {
     transpile: ['highlight.js', 'vue-docgen-api', 'draggable-resizable-vue3'],
   },
-  css: ['./src/assets/css/preflight.css', './src/assets/css/main.css'],
+  css: [join(currentDir, './src/assets/css/preflight.css'), join(currentDir, './src/assets/css/main.css')],
 })
